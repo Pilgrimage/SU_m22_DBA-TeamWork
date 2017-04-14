57db9999e56137bc2031123e7c8494cf38b47ce3
Pilgrimage <nanobot149@gmail.com>
2017-04-14 12:28:06 +0300

    Filled omissions in ExcavatorsContext file.

--- Excavators.Data/ExcavatorsContext.cs
+++ Excavators.Data/ExcavatorsContext.cs
@@ -29,30 +29,45 @@ namespace Excavators.Data
             //    .HasRequired(a => a.Excavator)
             //    .WithOptional()
             //    .WillCascadeOnDelete(false);
-
-
-
+            
             base.OnModelCreating(modelBuilder);
         }
 
+        public virtual DbSet<Excavator> Excavators { get; set; }
+        public virtual DbSet<RotorWheel> RotorWheels { get; set; }
         public virtual DbSet<Belt> Belts { get; set; }
         public virtual DbSet<Drum> Drums { get; set; }
-        public virtual DbSet<Excavator> Excavators { get; set; }
         public virtual DbSet<MRG> MRGs { get; set; }
+
         public virtual DbSet<Reducer> Reducers { get; set; }
+        public virtual DbSet<ReducerType> ReducerTypes { get; set; }
+
         public virtual DbSet<Motor> Motors { get; set; }
+        public virtual DbSet<MotorType> MotorTypes { get; set; }
+
         public virtual DbSet<TempSensor> TempSensors { get; set; }
+        public virtual DbSet<TempSensorType> TempSensorTypes { get; set; }
         public virtual DbSet<TempSensorData> TempSensorDatasSensorDatas { get; set; }
+
         public virtual DbSet<SpeedSensor> SpeedSensors { get; set; }
+        public virtual DbSet<SpeedSensorType> SpeedSensorTypes { get; set; }
         public virtual DbSet<SpeedSensorData> SpeedSensorDatas { get; set; }
+
         public virtual DbSet<CurrentSensor> CurrentSensors { get; set; }
+        public virtual DbSet<CurrentSensorType> CurrentSensorTypes { get; set; }
         public virtual DbSet<CurrentSensorData> CurrentSensorDatas { get; set; }
+
         public virtual DbSet<TensionSensor> TensionSensors { get; set; }
+        public virtual DbSet<TensionSensorType> TensionSensorTypes { get; set; }
         public virtual DbSet<TensionSensorData> TensionSensorDatas { get; set; }
+
         public virtual DbSet<ShiftingSensor> ShiftingSensors { get; set; }
+        public virtual DbSet<ShiftingSensorType> ShiftingSensorTypes { get; set; }
         public virtual DbSet<ShiftingSensorData> ShiftingSensorDatas { get; set; }
 
-
+        public virtual DbSet<VolumeSensor> VolumeSensors { get; set; }
+        public virtual DbSet<VolumeSensorType> VolumeSensorTypes { get; set; }
+        public virtual DbSet<VolumeSensorData> VolumeSensorDatas { get; set; }
 
     }
 
